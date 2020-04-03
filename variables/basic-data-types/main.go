82a414bcfd3dabbbc75aa5728209229888fa0fab
andreitch <andreitch.top@gmail.com>
2020-04-03 09:28:58 +0300

    basic data types

---
+++ variables/basic-data-types/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Print the literals
+//
+//  1. Print a few integer literals
+//
+//  2. Print a few float literals
+//
+//  3. Print true and false bool literals
+//
+//  4. Print your name using a string literal
+//
+//  5. Print a non-english sentence using a string literal
+//
+// ---------------------------------------------------------
+
+func main() {
+	fmt.Println(-1, 20, 0)
+	fmt.Println(-50.5, -0., 1.0, 10.65)
+	fmt.Println(true, false)
+	fmt.Println("Andrii")
+	fmt.Println("Merhaba, adım İnanç!")
+}
